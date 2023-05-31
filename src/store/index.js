e8907781d6dd4e205e238b42fd5baf9181876a93
Steven <steven@StevendeMacBook-Pro.local>
2023-05-31 07:58:27 -0400

    Add Redux Persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from 'redux-persist';
 import todoReducer from "./slice/todoSlice";
 import memberReducer from "./slice/memberSlice";
 import teamReducer from "./slice/teamSlice";
 import routeReducer from './slice/routeSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedTodoReducer = persistReducer(persistConfig, todoReducer);
+
 const store = configureStore({
   reducer: {
-    todo: todoReducer,
+    todo: persistedTodoReducer,
     member: memberReducer,
     team: teamReducer,
     route: routeReducer
@@ -14,3 +23,5 @@ const store = configureStore({
 });
 
 export default store;
+
+export const persistor = persistStore(store);
